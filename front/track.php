4d19123c5ec87c1bb20809d156bb1a1adb040161
Frederick Ennin <kpin463@gmail.com>
2023-02-07 16:13:10 +0000

    sdf

--- front/track.php
+++ front/track.php
@@ -180,7 +180,7 @@ if (isset($_GET['tracking'])) {
             $rh = customfetch('history', [['trackid', '=', $_GET['tracking']]], '', ['id' => 'DESC']);
 
             echo '<div style="margin: 5%" class="inner_main_agile_section">
-            <<div class="container">
+            <div class="container">
             <div class="card card-timeline px-2 border-none"> <ul class="bs4-order-tracking"> <li class="step active"> <div><i class="fas fa-user"></i></div> Order Placed </li> <li class="step active"> <div><i class="fas fa-bread-slice"></i></div> In transit </li> <li class="step"> <div><i class="fas fa-truck"></i></div> Out for delivery </li> <li class="step "> <div><i class="fas fa-birthday-cake"></i></div> Delivered </li> </ul> <h5 class="text-center"><b>In transit</b>. The order has been shipped!</h5>
             </div>
                 <h3 class="w3l_header w3_agileits_header">Shipment <span> Summary</span></h3>
