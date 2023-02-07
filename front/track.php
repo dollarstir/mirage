d2eeca1ef7839caf5fba9dbc716323aaebc4d599
Frederick Ennin <kpin463@gmail.com>
2023-02-07 16:10:10 +0000

    asdas

--- front/track.php
+++ front/track.php
@@ -6,7 +6,10 @@ begin();
 ?>
 
 
-<style>
+<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' rel='stylesheet'>
+  <link href='https://use.fontawesome.com/releases/v5.8.1/css/all.css' rel='stylesheet'>
+  <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+  <style>
     ::-webkit-scrollbar {
       width: 8px;
     }
@@ -100,7 +103,7 @@ begin();
     }
 
     .bs4-order-tracking li.active:after {
-      background: #ffb400
+      background:  #ffb400
     }
 
     .card-timeline {
