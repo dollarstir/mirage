7e24ef7581d6f891b473ec15b9a68d72e161fbd4
Frederick Ennin <kpin463@gmail.com>
2023-02-11 04:35:39 +0000

    asdas

--- back/login2.php
+++ back/login2.php
@@ -49,14 +49,14 @@ begin('Vault Authenticator'); ?>
 
             <div class="footer_part">
                 <div class="container">
-                    <div class="row">
+                    <!-- <div class="row">
                         <div class="col-lg-12">
                             <div class="footer_iner text-center">
                                 <p>2020 © Influence - Designed by <a href="#"> <i class="ti-heart"></i> </a><a href="#">
                                         Dashboard</a></p>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
     </section>
