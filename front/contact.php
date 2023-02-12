95f9643b95ad9978f59948a51024a4015c6cdb0d
Frederick Ennin <kpin463@gmail.com>
2023-02-12 23:03:16 +0000

    asas

--- front/contact.php
+++ front/contact.php
@@ -109,7 +109,7 @@ begin();
                                         <p>Fill The Form Bellow. And Get In Touch Immediately</p>
                                         <div class="contactform">
                                             <div id="message"></div>
-                                            <form  class="contact">
+                                            <form id="contactform"  class="contact">
                                                 <div class="row">
                                                     <div class="col-md-12">
                                                         <input type="text" placeholder="Complete Name" class="text-field input-style" id="name" name="name" />
